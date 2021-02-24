7eb93b307541ea68daf362e50ce3d7637a61815a
Naomi Chika <naomichika@marias-MacBook.local>
2021-02-24 21:57:37 +0300

    updated backend and frontend logic for calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
+//BUSINESS (BACK-END) LOGIC
 var add = function(number1, number2) {
   return number1 + number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+
 
 
 var subtract = function(number1, number2) {
   return number1 - number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
+
 
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
+
 
 var divide = function(number1, number2) {
   return number1 / number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+
+
+//USER INTERFACE (FRONT-END) LOGIC
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+  event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  $("#output").text(result)
+  });
+  });
